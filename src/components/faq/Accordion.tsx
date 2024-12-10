9dea523e1a8c5c23131601b7beda62ec5ca980d8
Sajib Hasan <sajib.thinkcrypt@gmail.com>
2024-12-10 18:30:15 +0600

    build error fix

--- src/components/faq/Accordion.tsx
+++ src/components/faq/Accordion.tsx
@@ -1,25 +1,25 @@
-import {
-	AccordionItem,
-	AccordionItemContent,
-	AccordionItemTrigger,
-	AccordionRoot,
-} from '@/components/ui/accordion';
-import { data } from '@/lib/config/data';
+// import {
+// 	AccordionItem,
+// 	AccordionItemContent,
+// 	AccordionItemTrigger,
+// 	AccordionRoot,
+// } from '@/components/ui/accordion';
+// import { data } from '@/lib/config/data';
 
-const AccordionComp = () => {
-	const { faq } = data;
-	return (
-		<AccordionRoot collapsible defaultValue={['item one']}>
-			{faq.map((item, index) => (
-				<AccordionItem key={index} value={item?.value}>
-					<AccordionItemTrigger cursor='pointer'>
-						{item?.question}
-					</AccordionItemTrigger>
-					<AccordionItemContent>{item?.answer}</AccordionItemContent>
-				</AccordionItem>
-			))}
-		</AccordionRoot>
-	);
-};
+// const AccordionComp = () => {
+// 	const { faq } = data;
+// 	return (
+// 		<AccordionRoot collapsible defaultValue={['item one']}>
+// 			{faq.map((item, index) => (
+// 				<AccordionItem key={index} value={item?.value}>
+// 					<AccordionItemTrigger cursor='pointer'>
+// 						{item?.question}
+// 					</AccordionItemTrigger>
+// 					<AccordionItemContent>{item?.answer}</AccordionItemContent>
+// 				</AccordionItem>
+// 			))}
+// 		</AccordionRoot>
+// 	);
+// };
 
-export default AccordionComp;
+// export default AccordionComp;
