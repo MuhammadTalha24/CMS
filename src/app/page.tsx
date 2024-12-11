f01c7556fc4d5dd8e7e659a9283f54c045960b15
Sajib Hasan <sajib.thinkcrypt@gmail.com>
2024-12-11 10:33:12 +0600

    slider add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,34 @@
-"use client";
+'use client';
 import {
-  IconSection,
-  IndustrySection,
-  PageLayout,
-  SectionPadding,
-} from "@/components";
-import { useColors } from "@/hooks";
-import { data } from "@/lib/config/data";
+	IconSection,
+	IndustrySection,
+	PageLayout,
+	SectionPadding,
+	SliderWithContent,
+} from '@/components';
+import { useColors } from '@/hooks';
+import { data } from '@/lib/config/data';
 
 const Page = () => {
-  const { colors } = useColors();
-  const { aboutSection, leadershipSection } = data;
-  return (
-    <PageLayout>
-      {/* <SliderWithContent sliderData={data?.hero} /> */}
-      <SectionPadding py={"3rem"} bg={colors?.black}>
-        <IconSection data={aboutSection} />
-      </SectionPadding>
-      <SectionPadding py={"3rem"} bg={colors?.black}>
-        <IconSection order data={leadershipSection} />
-      </SectionPadding>
-      <SectionPadding py={"3rem"} bg={colors?.black}>
-        <IndustrySection />
-      </SectionPadding>
-      {/* <SectionPadding py={'3rem'} bg={colors?.black}>
+	const { colors } = useColors();
+	const { aboutSection, leadershipSection } = data;
+	return (
+		<PageLayout>
+			<SliderWithContent sliderData={data?.hero} />
+			<SectionPadding py={'3rem'} bg={colors?.black}>
+				<IconSection data={aboutSection} />
+			</SectionPadding>
+			<SectionPadding py={'3rem'} bg={colors?.black}>
+				<IconSection order data={leadershipSection} />
+			</SectionPadding>
+			<SectionPadding py={'3rem'} bg={colors?.black}>
+				<IndustrySection />
+			</SectionPadding>
+			{/* <SectionPadding py={'3rem'} bg={colors?.black}>
 				<IndustrySection />
 			</SectionPadding> */}
-    </PageLayout>
-  );
+		</PageLayout>
+	);
 };
 
 export default Page;
