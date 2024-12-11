cfe16e50262c98098a98fe931f6f354a324463a8
SojibHasan <sojibsrd85@gmail.com>
2024-12-12 01:47:52 +0600

    hero section update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,66 @@
-'use client';
+"use client";
 import {
-	IconSection,
-	MissionVision,
-	LeadershipSection,
-	PageLayout,
-	SectionPadding,
-	SliderWithContent,
-	ValueTogether,
-	ServiceSection,
-	ConsultingSection,
-	IndustriesFaq,
-	CareerSection,
-	PartnerSection,
-} from '@/components';
-import { useColors } from '@/hooks';
-import { data } from '@/lib/config/data';
+  CareerSection,
+  ConsultingSection,
+  IconSection,
+  IndustriesFaq,
+  LeadershipSection,
+  MissionVision,
+  PageLayout,
+  PartnerSection,
+  SectionPadding,
+  ServiceSection,
+  SliderWithContent,
+  ValueTogether,
+} from "@/components";
+import { useColors } from "@/hooks";
+import { data } from "@/lib/config/data";
 
 const style = {
-	background: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url("/career/one.jpg")`,
-	backgroundPosition: 'center center',
-	backgroundRepeat: 'no-repeat',
-	backgroundSize: 'cover',
-	minHeight: 'auto',
+  background: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url("/career/one.jpg")`,
+  backgroundPosition: "center center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  minHeight: "auto",
 };
 
 const Page = () => {
-	const { colors } = useColors();
-	const { aboutSection, leadershipSection } = data;
-	return (
-		<PageLayout>
-			<SliderWithContent sliderData={data?.hero} />
-			<SectionPadding py={'3rem'} bg={colors?.black}>
-				<IconSection data={aboutSection} />
-			</SectionPadding>
-			<SectionPadding py={'3rem'} bg={colors?.black}>
-				<LeadershipSection order data={leadershipSection} />
-			</SectionPadding>
-			<SectionPadding py={'3rem'} bg={colors?.black}>
-				<MissionVision />
-			</SectionPadding>
-			<SectionPadding
-				py={'3rem'}
-				bg={colors?.black}
-				borderBottom={`1px solid ${colors?.primary}`}
-			>
-				<ValueTogether />
-			</SectionPadding>
-			<SectionPadding py={'3rem'} bg={colors?.black}>
-				<ServiceSection />
-			</SectionPadding>
-			<ConsultingSection />
-			<SectionPadding py={'3rem'} bg={colors?.black}>
-				<IndustriesFaq />
-			</SectionPadding>
-			<SectionPadding py={'6rem'} style={style}>
-				<CareerSection />
-			</SectionPadding>
-			<SectionPadding py={'3rem'}>
-				<PartnerSection />
-			</SectionPadding>
-		</PageLayout>
-	);
+  const { colors } = useColors();
+  const { aboutSection, leadershipSection } = data;
+  return (
+    <PageLayout>
+      <SliderWithContent sliderData={data?.hero} />
+      <SectionPadding py={"3rem"} bg={colors?.black}>
+        <IconSection data={aboutSection} />
+      </SectionPadding>
+      <SectionPadding py={"3rem"} bg={colors?.black}>
+        <LeadershipSection order data={leadershipSection} />
+      </SectionPadding>
+      <SectionPadding py={"3rem"} bg={colors?.black}>
+        <MissionVision />
+      </SectionPadding>
+      <SectionPadding
+        py={"3rem"}
+        bg={colors?.black}
+        borderBottom={`1px solid ${colors?.primary}`}
+      >
+        <ValueTogether />
+      </SectionPadding>
+      <SectionPadding py={"3rem"} bg={colors?.black}>
+        <ServiceSection />
+      </SectionPadding>
+      <ConsultingSection />
+      <SectionPadding py={"3rem"} bg={colors?.black}>
+        <IndustriesFaq />
+      </SectionPadding>
+      <SectionPadding py={"6rem"} style={style}>
+        <CareerSection />
+      </SectionPadding>
+      <SectionPadding py={"3rem"}>
+        <PartnerSection />
+      </SectionPadding>
+    </PageLayout>
+  );
 };
 
 export default Page;
--- src/components/slider/SliderWithContent.tsx
+++ src/components/slider/SliderWithContent.tsx
@@ -1,217 +1,185 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-'use client';
-import { Heading, NormalText, Overlay } from '@/components';
-import { useColors } from '@/hooks';
-import { zIndex } from '@/lib/config/constants';
-// import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons';
-import { Box, Image } from '@chakra-ui/react'; // If you're using Chakra UI
-import { FC } from 'react';
-import SlickSlider from 'react-slick';
-import 'slick-carousel/slick/slick-theme.css';
-import 'slick-carousel/slick/slick.css';
+"use client";
+import { Heading, Icon, NormalText, Overlay } from "@/components";
+import { useColors } from "@/hooks";
+import { zIndex } from "@/lib/config/constants";
+import { Box, Center, Image } from "@chakra-ui/react"; // If you're using Chakra UI
+import { FC, useRef } from "react";
+import SlickSlider from "react-slick";
 
-// const styles = {
-// 	buttonStyle: {
-// 		ariaLabel: 'Previous',
-// 		position: 'absolute',
-// 		top: '50%',
-// 		transform: 'translateY(-50%)',
-// 		zIndex: '10',
-// 		bg: 'rgba(0, 0, 0, 0.5)',
-// 		color: 'white',
-// 		borderRadius: 'full',
-// 		p: { base: '18px', sm: '8px', md: '12px' },
-// 		size: {
-// 			base: 'sm', // Adjust size for small screens
-// 			sm: 'lg',
-// 		},
-// 	},
-// };
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
 
 // Custom Previous Button
-// const PreviousButton = ({ onClick }: { onClick?: () => void }) => {
-// 	return (
-// 		<IconButton
-// 			aria-label='Previous'
-// 			// icon={<ArrowBackIcon />}
-// 			position='absolute'
-// 			left={{ base: '20px', sm: '50px', md: '150px', lg: '200px', xl: '245px' }}
-// 			top='50%'
-// 			transform='translateY(-50%)'
-// 			zIndex='10'
-// 			bg='rgba(0, 0, 0, 0.5)'
-// 			color='white'
-// 			borderRadius='full'
-// 			onClick={onClick}
-// 			_hover={{ bg: 'rgba(0, 0, 0, 0.8)' }}
-// 			p={{ base: '18px', sm: '8px', md: '12px' }}
-// 			size={{
-// 				base: 'sm', // Adjust size for small screens
-// 				sm: 'lg',
-// 			}}
-// 		/>
-// 	);
-// };
+const PreviousButton = ({ onClick }: { onClick?: () => void }) => {
+  const { colors } = useColors();
+  return (
+    <Center
+      position="absolute"
+      bottom="18%"
+      right="16%"
+      width="40px"
+      h="40px"
+      zIndex={zIndex?.hero}
+      cursor="pointer"
+      onClick={onClick} // Pass onClick here
+    >
+      <Icon color={colors?.white} name="arrowLeft" />
+    </Center>
+  );
+};
+
+const NextButton = ({ onClick }: { onClick?: () => void }) => {
+  const { colors } = useColors();
+  return (
+    <Center
+      position="absolute"
+      bottom="10%"
+      right="16%"
+      width="40px"
+      h="40px"
+      zIndex={zIndex?.hero}
+      cursor="pointer"
+      onClick={onClick} // Pass onClick here
+    >
+      <Icon color={colors?.white} name="arrowRight" />
+    </Center>
+  );
+};
 
 // Custom Next Button
-// const NextButton = ({ onClick }: { onClick?: () => void }) => {
-// 	return (
-// 		<IconButton
-// 			aria-label='Next'
-// 			// icon={<Icon name='search' />}
-// 			position='absolute'
-// 			right={{
-// 				base: '20px',
-// 				sm: '50px',
-// 				md: '150px',
-// 				lg: '200px',
-// 				xl: '245px',
-// 			}}
-// 			top='50%'
-// 			transform='translateY(-50%)'
-// 			zIndex='10'
-// 			bg='rgba(0, 0, 0, 0.5)'
-// 			color='white'
-// 			borderRadius='full'
-// 			onClick={onClick}
-// 			_hover={{ bg: 'rgba(0, 0, 0, 0.8)' }}
-// 			p={{ base: '18px', sm: '8px', md: '12px' }}
-// 			size={{
-// 				base: 'sm', // Adjust size for small screens
-// 				sm: 'lg',
-// 			}}
-// 		/>
-// 	);
-// };
 
 type SliderItem = {
-	heading: string;
-	text?: string;
-	image: string;
-	textlist?: string[];
+  heading: string;
+  text?: string;
+  image: string;
+  textlist?: string[];
 };
 type SliderData = {
-	one: SliderItem;
-	two: SliderItem;
-	three: SliderItem;
-	four: SliderItem;
+  one: SliderItem;
+  two: SliderItem;
+  three: SliderItem;
+  four: SliderItem;
 };
 
 type SliderWithContentProps = {
-	sliderData: SliderData;
+  sliderData: SliderData;
 };
 
 const SliderWithContent: FC<SliderWithContentProps> = ({ sliderData }) => {
-	const { colors } = useColors();
-	const valuesMap = Object.values(sliderData);
+  const { colors } = useColors();
+  const sliderRef = useRef<SlickSlider | null>(null);
+  const valuesMap = Object.values(sliderData);
 
-	const settings = {
-		centerMode: true,
-		centerPadding: '300px',
-		autoplay: true,
-		autoplaySpeed: 3000,
-		pauseOnHover: true,
-		slidesToShow: 1,
-		infinite: true,
-		speed: 1500,
-		// prevArrow: <PreviousButton />,
-		// nextArrow: <Icon name='arrow' />,
-		responsive: [
-			{
-				breakpoint: 1536,
-				settings: {
-					slidesToShow: 1,
-					centerPadding: '300px',
-				},
-			},
-			{
-				breakpoint: 1280,
-				settings: {
-					slidesToShow: 1,
-					centerPadding: '250px',
-				},
-			},
-			{
-				breakpoint: 992,
-				settings: {
-					slidesToShow: 1,
-					centerPadding: '200px',
-				},
-			},
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 1,
-					centerPadding: '100px',
-				},
-			},
-			{
-				breakpoint: 480, // For small devices
-				settings: {
-					slidesToShow: 1,
-					centerPadding: '0px',
-				},
-			},
-		],
-	};
+  const settings = {
+    centerMode: true,
+    centerPadding: "300px",
+    autoplay: false,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    slidesToShow: 1,
+    infinite: true,
+    speed: 1500,
+    prevArrow: (
+      <PreviousButton onClick={() => sliderRef.current?.slickPrev()} />
+    ),
+    nextArrow: <NextButton onClick={() => sliderRef.current?.slickNext()} />,
+    responsive: [
+      {
+        breakpoint: 1536,
+        settings: {
+          slidesToShow: 1,
+          centerPadding: "300px",
+        },
+      },
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 1,
+          centerPadding: "250px",
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          centerPadding: "200px",
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          centerPadding: "100px",
+        },
+      },
+      {
+        breakpoint: 480, // For small devices
+        settings: {
+          slidesToShow: 1,
+          centerPadding: "0px",
+        },
+      },
+    ],
+  };
 
-	return (
-		<Box overflowX='hidden' h='full' w='full'>
-			<SlickSlider {...settings}>
-				{valuesMap.map((item, index) => (
-					<Box key={index} px='20px' overflow='hidden'>
-						<Box position={item?.heading && 'relative'} h='80vh'>
-							<Image
-								src={item?.image}
-								alt={`slide-${index}`}
-								borderRadius='md'
-								boxShadow='md'
-								objectFit='cover'
-								w='full'
-								h='full'
-							/>
-							{/* {item.heading && <Overlay />} */}
-							{item?.heading && <Overlay />}
+  return (
+    <Box overflowX="hidden" h="full" w="full" mt="80px">
+      <SlickSlider ref={sliderRef} {...settings}>
+        {valuesMap.map((item, index) => (
+          <Box key={index} px="20px" overflow="hidden">
+            <Box position={item?.heading && "relative"} h="80vh">
+              <Image
+                src={item?.image}
+                alt={`slide-${index}`}
+                borderRadius="md"
+                boxShadow="md"
+                objectFit="cover"
+                w="full"
+                h="full"
+              />
+              {/* {item.heading && <Overlay />} */}
+              {item?.heading && <Overlay />}
 
-							{item?.heading && (
-								<Box
-									zIndex={zIndex?.hero}
-									position='absolute'
-									w='70%'
-									top='50%'
-									left='50%'
-									transform={'translate(-50%, -50%)'}
-								>
-									<Heading
-										fontSize={{ base: '1.5rem', lg: '3rem' }}
-										fontWeight='600'
-										color={colors?.white}
-										textAlign={'center'}
-									>
-										{item?.heading}
-									</Heading>
-									{item?.text && (
-										<NormalText fontSize='1.5rem' textAlign={'center'}>
-											{item?.text}
-										</NormalText>
-									)}
-									{item?.textlist?.map((item: any, i: number) => (
-										<NormalText
-											key={i}
-											fontSize={{ base: '1.5rem', lg: '1.5rem' }}
-											color={colors?.white}
-										>
-											{`• ${item}`}
-										</NormalText>
-									))}
-								</Box>
-							)}
-						</Box>
-					</Box>
-				))}
-			</SlickSlider>
-		</Box>
-	);
+              {item?.heading && (
+                <Box
+                  zIndex={zIndex?.hero}
+                  position="absolute"
+                  w="70%"
+                  top="50%"
+                  left="50%"
+                  transform={"translate(-50%, -50%)"}
+                >
+                  <Heading
+                    fontSize={{ base: "1.5rem", lg: "3rem" }}
+                    fontWeight="600"
+                    color={colors?.white}
+                    textAlign={"center"}
+                  >
+                    {item?.heading}
+                  </Heading>
+                  {item?.text && (
+                    <NormalText fontSize="1.5rem" textAlign={"center"}>
+                      {item?.text}
+                    </NormalText>
+                  )}
+                  {item?.textlist?.map((item: any, i: number) => (
+                    <NormalText
+                      key={i}
+                      fontSize={{ base: "1.5rem", lg: "1.5rem" }}
+                      color={colors?.white}
+                    >
+                      {`• ${item}`}
+                    </NormalText>
+                  ))}
+                </Box>
+              )}
+            </Box>
+          </Box>
+        ))}
+      </SlickSlider>
+    </Box>
+  );
 };
 
 export default SliderWithContent;
